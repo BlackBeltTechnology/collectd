80fe7af1f70d5e3b677da68ab93d1bb78d0974ac
Börcsök József <jozsef.borcsok@avon.com>
2016-11-22 15:06:59 +0100

    add stats counters

--- collectd-core/src/main/java/org/collectd/services/UdpPacketSender.java
+++ collectd-core/src/main/java/org/collectd/services/UdpPacketSender.java
@@ -8,6 +8,8 @@ import java.net.InetSocketAddress;
 import java.net.MulticastSocket;
 import java.net.SocketException;
 import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 import lombok.extern.slf4j.Slf4j;
 import org.collectd.config.CollectdConstants;
 import org.collectd.model.Notification;
@@ -27,8 +29,11 @@ public class UdpPacketSender {
 
     private DatagramSocket socket;
     private MulticastSocket mcast;
-    
+
     private String client;
+
+    private final AtomicLong bytesSent = new AtomicLong(0L);
+    private final AtomicInteger packetsSent = new AtomicInteger(0);
 
     /**
      * Create new UDP packet writer instance. Default packet size is used.
@@ -60,12 +65,12 @@ public class UdpPacketSender {
      */
     public void send(final Values values) throws IOException {
         setDefaults(values);
-        
+
         final byte[] bufferToSend = writer.checkSpace(values);
         if (bufferToSend != null) {
             flush(bufferToSend);
         }
-        
+
         writer.writeValuesPart(values);
     }
 
@@ -77,17 +82,25 @@ public class UdpPacketSender {
      */
     public void send(final Notification notification) throws IOException {
         setDefaults(notification);
-        
+
         final byte[] bufferToSend = writer.checkSpace(notification);
         if (bufferToSend != null) {
             flush(bufferToSend);
         }
-        
+
         writer.writeNotificationPart(notification);
     }
 
     public void flush() throws IOException {
         flush(writer.getBuffer());
+    }
+
+    public long getBytesSent() {
+        return bytesSent.longValue();
+    }
+
+    public int getPacketsSent() {
+        return packetsSent.intValue();
     }
 
     private String getClient() {
@@ -125,12 +138,15 @@ public class UdpPacketSender {
         }
         return mcast;
     }
-    
+
     private void flush(final byte[] buffer) throws IOException {
         final int length = buffer.length;
         if (length == 0) {
             return;
         }
+
+        bytesSent.addAndGet(length);
+        packetsSent.incrementAndGet();
 
         if (log.isDebugEnabled()) {
             log.debug("Sending UDP packet, buffer length: " + length);
